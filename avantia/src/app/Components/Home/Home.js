c0b5e31cf191a4551e224ee589a46594b0f29a4a
WIsnerSL <122920829+WIsnerSL@users.noreply.github.com>
2023-10-02 16:38:26 -0300

    Construindo o home

--- avantia/src/app/Components/Home/Home.js
+++ avantia/src/app/Components/Home/Home.js
@@ -1,7 +1,21 @@
-export default function Home() {
-    return <section>
+import '../Home/Home.css'
 
-        <h1> Home</h1>
-    </section>
+export default function Home() {
+    
+    return <section className='HomeBackGround'>
+                <div className='Mov-Text'>
+                    <div className='Text-Ideias'>Você tem ideias para o seu negócio?</div>
+                    <div className='text-Realidade'>Então nos deixe transformá-los em realidade!</div>
+                </div>
+                    <img src='Avantia negro.svg'/>
+                <div className='Mov-logoAvantia'>
+                    <div className='Logo-Avantia'>
+
+                    </div>
+                </div>
+                <div>
+                    <button>Quero Agir!</button>
+                </div>
+           </section>
 
 }
