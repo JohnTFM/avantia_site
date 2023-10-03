c02aa55db447419aa0ecf7c2ea596802bff80304
WIsnerSL <122920829+WIsnerSL@users.noreply.github.com>
2023-10-03 11:49:58 -0300

    Quem Somos

--- avantia/src/app/Components/Home/Home.js
+++ avantia/src/app/Components/Home/Home.js
@@ -1,21 +1,23 @@
 import '../Home/Home.css'
 
+
+
+
+
 export default function Home() {
     
     return <section className='HomeBackGround'>
-                <div className='Mov-Text'>
-                    <div className='Text-Ideias'>Você tem ideias para o seu negócio?</div>
-                    <div className='text-Realidade'>Então nos deixe transformá-los em realidade!</div>
-                </div>
-                    <img src='Avantia negro.svg'/>
-                <div className='Mov-logoAvantia'>
-                    <div className='Logo-Avantia'>
 
+                    <div className='Mov-Text'>
+                        <div className='Text-Ideias'>Você tem ideias para o seu negócio?</div>
+                        <div className='text-Realidade'>Então nos deixe transformá-las em realidade!</div>
+                        <button className='QroAgir'> <p className='MovQroAgir'>Quero Agir!</p> 
+                            <img src='/imagens/Vector.png' className='ImgVector'/>
+                        </button>
+                    </div>                
+                    <div className='Mov-logoAvantia'>
+                            <img src='/imagens/logo.png' className='LogoHome'></img>
                     </div>
-                </div>
-                <div>
-                    <button>Quero Agir!</button>
-                </div>
            </section>
 
 }
